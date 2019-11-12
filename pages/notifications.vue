182fa25d8cfb79b6b6b4ac7c9b2a5cae97158e76
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-11-12 16:11:25 +0800

    EXPERIMENTAL: able to fetch data using jsonplaceholder

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -1,17 +1,34 @@
 <template>
   <v-container>
-    <v-row >
+    <v-row>
       <v-col align-self="start" class="text-left">Notifications</v-col>
       <v-col class="text-right" align-self="end">
         <v-btn small outlined :ripple="false">Clear All</v-btn>
       </v-col>
     </v-row>
+    <v-list>
+      <v-list-item v-for="todo in todos" :key="todo.id">
+        <v-card>
+          <v-card-title v-text="todo.title"></v-card-title>
+        </v-card>
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get(
+      'https://jsonplaceholder.typicode.com/todos?_limit=10'
+    )
+    console.log(data)
+    return {
+      todos: data
+    }
+  }
+})
 </script>
 <style>
 </style>
