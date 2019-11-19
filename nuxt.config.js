ea50628b8b34aac123a653cccc4ff97b2e01ebfe
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-11-19 09:37:34 +0800

    prettier format for nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,54 +2,54 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_description,
     title: process.env.npm_package_description || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     '@nuxtjs/vuetify',
     '@nuxt/typescript-build',
-    'nuxt-typed-vuex',
+    'nuxt-typed-vuex'
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa',
+    '@nuxtjs/pwa'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
     baseURL: process.env.PORT || 'http://localhost:3000'
   },
@@ -58,18 +58,16 @@ export default {
    */
 
   /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     global: {
       ripple: false
     },
     theme: {
-
       themes: {
-
         light: {
           primary: colors.green.lighten1,
           accent: colors.grey.lighten3,
@@ -77,8 +75,7 @@ export default {
           info: colors.teal.darken1,
           warning: colors.amber.base,
           error: colors.red.darken2,
-          success: colors.green.accent3,
-
+          success: colors.green.accent3
         },
         dark: {
           primary: colors.green.lighten1,
@@ -89,20 +86,17 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
-      },
-    },
+      }
+    }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    transpile: [
-      /nuxt-typed-vuex/,
-    ],
+    transpile: [/nuxt-typed-vuex/],
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
