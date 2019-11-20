0b66b685c267d8d63268df82140007c08482c713
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-11-20 22:27:15 +0800

    :art: Separate Image to a new Component

--- components/LoginComponent/Images.vue
+++ components/LoginComponent/Images.vue
@@ -1,12 +1,13 @@
 <template>
-  
+  <v-col xl="6" lg="6" sm="12" md="12" xs="12" class="d-none d-lg-flex d-xl-none">
+    <v-img :src="require('@/assets/login.svg')" max-height="350" aspect-ratio="1" contain></v-img>
+  </v-col>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    
+  name: 'Images'
 })
 </script>
 <style>
-
 </style>
