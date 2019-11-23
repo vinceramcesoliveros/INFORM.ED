9281a5b0a1490e90c33a4b2a41cc6fdc79519a90
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-11-23 22:57:10 +0800

    :sparkle: Prettier format

--- store/index.ts
+++ store/index.ts
@@ -1,38 +1,43 @@
-import { getAccessorType, mutationTree, getterTree, actionTree, } from 'nuxt-typed-vuex'
-import * as UI from './UI';
-import * as comments from './comments';
-import * as posts from './posts';
+import {
+  getAccessorType,
+  mutationTree,
+  getterTree,
+  actionTree
+} from 'nuxt-typed-vuex'
+import * as UI from './UI'
+import * as comments from './comments'
+import * as posts from './posts'
+import * as accounts from './accounts'
 // Disable error logging when you violate the Vuex rules
 // Because only mutations can update the state and not the UI itself
-export const strict = false;
+export const strict = false
 
 export const state = () => ({
-    data: null as null | string,
-});
+  data: null as null | string
+})
 
 export type RootState = ReturnType<typeof state>
 export const mutations = mutationTree(state, {
-    SET_ERROR(state: RootState, data: string) {
-        state.data = data;
-    }
-});
+  SET_ERROR(state: RootState, data: string) {
+    state.data = data
+  }
+})
 export const getters = getterTree(state, {
-    GET_ERRORS(state: RootState) {
-        return state.data;
-    },
-
-});
-export const actions = actionTree({ state, getters, mutations }, {});
+  GET_ERRORS(state: RootState) {
+    return state.data
+  }
+})
+export const actions = actionTree({ state, getters, mutations }, {})
 
 export const accessorType = getAccessorType({
-    state,
-    actions,
-    mutations,
-    getters,
-    modules: {
-        UI,
-        comments,
-        posts,
-    }
-
+  state,
+  actions,
+  mutations,
+  getters,
+  modules: {
+    UI,
+    comments,
+    posts,
+    accounts
+  }
 })
