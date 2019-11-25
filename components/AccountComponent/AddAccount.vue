d733f421a9e32975295ebb6be51939be26c52d79
Vince Ramces Oliveros <blusea231@gmail.com>
2019-11-25 21:46:37 +0800

    :art: snackbar to account

--- components/AccountComponent/AddAccount.vue
+++ components/AccountComponent/AddAccount.vue
@@ -1,34 +1,39 @@
 <template>
-  <v-dialog v-model="dialog" max-width="600">
-    <template v-slot:activator="{on}">
-      <v-btn v-on="on" outlined :ripple="false">
-        <v-icon>mdi-plus</v-icon>Account
-      </v-btn>
-    </template>
-    <v-form @submit.prevent="addAccount" v-model="form">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Account</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" md="12" sm="12">
-                <v-text-field label="name" v-model="name"></v-text-field>
-              </v-col>
-              <v-col cols="12" md="12" sm="12">
-                <v-text-field label="email" v-model="email"></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="secondary" type="submit">Add Account</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-form>
-  </v-dialog>
+  <div>
+    <v-dialog v-model="dialog" max-width="600">
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" outlined :ripple="false">
+          <v-icon>mdi-plus</v-icon>Account
+        </v-btn>
+      </template>
+      <v-form @submit.prevent="addAccount" v-model="valid">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Account</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="12" sm="12">
+                  <v-text-field label="name" v-model="name"></v-text-field>
+                </v-col>
+                <v-col cols="12" md="12" sm="12">
+                  <v-text-field label="email" v-model="email"></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="secondary" type="submit">Add Account</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
+    <v-snackbar v-model="snackbar" :timeout="5000">
+      {{ message }}
+    </v-snackbar>
+  </div>
 </template>
 
 <script lang="ts">
@@ -40,19 +45,27 @@ export default Vue.extend({
       valid: false,
       name: '',
       email: '',
-      dialog: false
+      dialog: false,
+      snackbar: false,
+      message: 'Successfully Added'
     }
   },
   methods: {
     addAccount() {
+      this.snackbar = true
       this.$accessor.accounts.ADD_ACCOUNT({
         name: this.name,
         email: this.email
       })
+      this.closeDialog()
+    },
+    closeDialog() {
+      this.dialog = false
+      this.email = ''
+      this.name = ''
     }
   }
 })
 </script>
 
-<style>
-</style>
+<style></style>
