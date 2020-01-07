73db2f7c2adeb19f02d8bd16fb73fddb63d82355
Vince Ramces V. Oliveros <blusea231@gmail.com>
2020-01-07 08:05:54 +0800

    change min-width to percentage for responsive layout

--- pages/subjects.vue
+++ pages/subjects.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout>
-    <v-skeleton-loader  min-width="35rem" class="mx-auto mt-5" type="article"></v-skeleton-loader>
+    <v-skeleton-loader min-width="97%" max-width="90%" class="mx-auto mt-5" type="article"></v-skeleton-loader>
   </v-layout>
 </template>
 
